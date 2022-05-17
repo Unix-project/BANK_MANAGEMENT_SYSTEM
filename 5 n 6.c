a4f1e4e719435cec631bb7542bf9da3fcbdaa227
R20EF272 <93900414+R20EF272@users.noreply.github.com>
2022-05-17 20:46:17 +0530

    modified 5 n 6

---
+++ 5 n 6.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <stdbool.h>
+int i, j;
+int main_exit;
+void menu();
+int atm();
+void close();
+
+struct date
+{
+    int month, day, year;
+};
+struct
+{
+
+    char name[60];
+    int acc_no, age;
+    char address[60];
+    char citizenship[15];
+    double phone;
+    char acc_type[10];
+    float amt;
+    struct date dob;
+    struct date deposit;
+    struct date withdraw;
+
+} add, upd, check, rem, transaction;
+
+
+void fordelay(int j)
+{
+    int i, k;
+    for (i = 0; i < j; i++)
+        k = i;
+}
+
+int main()
+
+{
+    int choice, test = 0;
+    FILE *old, *newrec;
+    old = fopen("record.dat", "r");
+    newrec = fopen("new.dat", "w");
+
+    printf("Enter the account no. of the customer:");
+    scanf("%d", &transaction.acc_no);
+    while (fscanf(old, "%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d", &add.acc_no, add.name, &add.dob.month, &add.dob.day, &add.dob.year, &add.age, add.address, add.citizenship, &add.phone, add.acc_type, &add.amt, &add.deposit.month, &add.deposit.day, &add.deposit.year) != EOF)
+    {
+
+        if (add.acc_no == transaction.acc_no)
+        {
+            test = 1;
+            if (strcmpi(add.acc_type, "fixed1") == 0 || strcmpi(add.acc_type, "fixed2") == 0 || strcmpi(add.acc_type, "fixed3") == 0)
+            {
+                printf("\a\a\a\n\nYOU CANNOT DEPOSIT OR WITHDRAW CASH IN FIXED ACCOUNTS!!!!!");
+                fordelay(1000000000);
+                system("cls");
+                menu();
+            }
+            printf("\n\nDo you want to\n1.Deposit\n2.Withdraw?\n\nEnter your choice(1 for deposit and 2 for withdraw):");
+            scanf("%d", &choice);
+            if (choice == 1)
+            {
+                printf("Enter the amount you want to deposit:₹ ");
+                scanf("%f", &transaction.amt);
+                add.amt += transaction.amt;
+                fprintf(newrec, "%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n", add.acc_no, add.name, add.dob.month, add.dob.day, add.dob.year, add.age, add.address, add.citizenship, add.phone, add.acc_type, add.amt, add.deposit.month, add.deposit.day, add.deposit.year);
+                printf("\n\nDeposited successfully!");
+            }
+            else
+            {
+                printf("Enter the amount you want to withdraw:₹ ");
+                scanf("%f", &transaction.amt);
+                add.amt -= transaction.amt;
+                fprintf(newrec, "%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n", add.acc_no, add.name, add.dob.month, add.dob.day, add.dob.year, add.age, add.address, add.citizenship, add.phone, add.acc_type, add.amt, add.deposit.month, add.deposit.day, add.deposit.year);
+                printf("\n\nWithdrawn successfully!");
+            }
+        }
+        else
+        {
+            fprintf(newrec, "%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n", add.acc_no, add.name, add.dob.month, add.dob.day, add.dob.year, add.age, add.address, add.citizenship, add.phone, add.acc_type, add.amt, add.deposit.month, add.deposit.day, add.deposit.year);
+        }
+    }
+    fclose(old);
+    fclose(newrec);
+    remove("record.dat");
+    rename("new.dat", "record.dat");
+    if (test != 1)
+    {
+        printf("\n\nRecord not found!!");
+    transact_invalid:
+        printf("\n\n\nEnter 0 to try again,1 to return to main menu and 2 to exit:");
+        scanf("%d", &main_exit);
+        system("cls");
+        if (main_exit == 0)
+            main();
+        else if (main_exit == 1)
+            menu();
+        else if (main_exit == 2)
+            close();
+        else
+        {
+            printf("\nInvalid!");
+            goto transact_invalid;
+        }
+    }
+    else
+    {
+        printf("\nEnter 1 to go to the main menu and 0 to exit:");
+        scanf("%d", &main_exit);
+        system("cls");
+        if (main_exit == 1)
+            menu();
+        else
+            close();
+    }
+}
+
+void menu(void)
+{   char choice;
+    system("cls");
+    system("color 7");
+    printf("\n\n\t\t\t       BANKING MANAGEMENT SYSTEM");
+    printf("\n\n\n\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 WELCOME TO THE MAIN MENU \xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\n\n\t\tUpdate information of existing account\n\t\t Yes/No\n\n\n\t\t Enter your choice:Y for Yes and N for No:");
+    scanf("%s",&choice);
+
+    system("cls");
+
+    if(choice=='Y')
+    {
+    	main();
+	}
+	else if(choice=='N'){
+		close();
+	}
+	else{
+		printf("Invalid choice try again");
+		menu();
+	}
+
+
+
+}
