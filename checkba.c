e1660164340558fc75bd9153de9e4c95c50c5b31
Jothika20 <85340452+Jothika20@users.noreply.github.com>
2022-05-18 07:44:19 +0530

    Update checkba.c

--- checkba.c
+++ checkba.c
@@ -3,9 +3,9 @@
 #include<windows.h>
 #include<stdbool.h>
 int i,j;
-int main_exit;
+
 void fordelay(int);
-void view_list();
+
 struct date{
     int month,day,year;
 
@@ -18,7 +18,7 @@ struct {
     char citizenship[15];
     double phone;
     char acc_type[10];
-    float amt;
+    float amt;	
     struct date dob;
     struct date deposit;
     struct date withdraw;
@@ -26,62 +26,51 @@ struct {
     }add,upd,check,rem,transaction;
 
 int main(float balance);
-void menuExit();
 
 
 
 
-void view_list()
-{
-    FILE *view;
-    view=fopen("record.dat","r");
-    int test=0;
-    system("cls");
-    printf("\nACC. NO.\tNAME\t\t\tADDRESS\t\t\tPHONE\n");
 
-    while(fscanf(view,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d",&add.acc_no,add.name,&add.dob.month,&add.dob.day,&add.dob.year,&add.age,add.address,add.citizenship,&add.phone,add.acc_type,&add.amt,&add.deposit.month,&add.deposit.day,&add.deposit.year)!=EOF)
-       {
-           printf("\n%6d\t %10s\t\t\t%10s\t\t%.0lf",add.acc_no,add.name,add.address,add.phone);
-           test++;
-       }
 
-    fclose(view);
-    if (test==0)
-        {   system("cls");
-            printf("\nNO RECORDS!!\n");}
-
-    
-}
 
 int main(float balance) {
+	
 	float number = 1234;
-  int pass;
-  
-  printf("\n\n\t\t\tEnter the PIN No:");
-  scanf("%d", &pass);
-
-  if (pass == number)
+	int acc_no;
+	int test=0;
+    FILE *view;
+	view=fopen("record.dat","r");
+    printf("\n\n\t\t\tEnter the account number:");
+    scanf("%d", &check.acc_no);
+	
+	
+    while(fscanf(view,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d",&add.acc_no,add.name,&add.dob.month,&add.dob.day,&add.dob.year,&add.age,add.address,add.citizenship,&add.phone,add.acc_type,&add.amt,&add.deposit.month,&add.deposit.day,&add.deposit.year)!=EOF)
     {
-      printf("\n\nPIN Matched!\nLOADING");
-      	printf("You Choose to See your Balance\n");
-    	printf("****Your Available Balance is:   $%.2f\n\n", balance);
-        for(i=0;i<=6;i++)
+    	if(check.acc_no==add.acc_no)
+		{
+    	printf("\t\t*****Your Account Balance is*****\n");
+    	printf("\t\t\t\t%.2f\n\n", add.amt);
+    	
+    	
+            
+        
+    	printf("\t\t*****Your details are as follows*****\n");
+    	printf("\nACC. NO\tNAME\t\t\tADDRESS\t\t\tPHONE\n");
+    	printf("\n%6d\t %10s\t\t\t%10s\t\t%.0lf\n",add.acc_no,add.name,add.address,add.phone);
+    	for(i=0;i<=6;i++)
         {
-            fordelay(1000000000);
+            fordelay(10000000000);
             printf(".");
         }
-        system("cls");
-       
-   	view_list();
+    	test++;
     }
-  else
-    {
-      printf("Invalid\n Try Again");
-      
-    }
-	
-   
-    
+    	
+		
+}
+		
+		fclose(view);
+		system("cls");
+  
 }
 
 void fordelay(int j)
@@ -89,4 +78,3 @@ void fordelay(int j)
     for(i=0;i<j;i++)
          k=i;
 }
-
